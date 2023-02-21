c8cd74bb81992ccd0896253b28f59326d3d85337
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-02-22 02:54:07 +0300

    1.7. Конструктор [#173889].

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Error is active = " + active);
+        System.out.println("Error status = " + status);
+        System.out.println("Error message = " + message);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Error emptyError = new Error();
+        Error forbiddenError = new Error(true, 403, "Forbidden");
+        Error gatewayTimeoutError = new Error(false, 504, "Gateway Timeout");
+        emptyError.printInfo();
+        forbiddenError.printInfo();
+        gatewayTimeoutError.printInfo();
+    }
+}
