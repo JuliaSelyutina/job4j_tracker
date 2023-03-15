8fdee869b8fdfad13aa14b5134872c716451ba3c
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-03-15 20:42:42 +0300

    Revert "2. Перегрузка конструктора [#173890]."
    This reverts commit ec713e0f6b0a40b622cdf2c1fa56b481fb702593.

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,29 +1,8 @@
 package ru.job4j.tracker;
 
 public class Item {
-    /*
-    2. Создайте три конструктора:
-    без параметров,
-    с параметром String name,
-    с параметрами int id и String name.
-
-    Конструкторы с параметрами должны инициализировать поля, которые объявлены в классе
-     */
     private int id;
     private String name;
-
-    public Item() {
-
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public Item(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public int getId() {
         return id;
