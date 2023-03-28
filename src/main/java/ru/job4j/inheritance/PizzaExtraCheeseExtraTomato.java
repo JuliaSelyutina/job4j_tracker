eb9fb4216237133d2737997d09946f00fcab51ad
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-03-28 21:20:30 +0300

    6. Вызов переопределенного метода родителя - super.method ... [#504871].

---
+++ src/main/java/ru/job4j/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,15 @@
+package ru.job4j.inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
+    private static final String TOMATO = " + extra tomato";
+
+    public PizzaExtraCheeseExtraTomato() {
+        super();
+    }
+
+    @Override
+    public String name() {
+        return super.name() + TOMATO;
+    }
+
+}
