89f91c565793d26c04bbe2ebd05f5f21437e972b
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-03-31 23:09:31 +0300

    3.1. Массив с пустыми ячейками [#176470].

--- src/main/java/ru/job4j/pojo/Store.java
+++ src/main/java/ru/job4j/pojo/Store.java
@@ -5,26 +5,14 @@ public class Store {
         Product milk = new Product("Milk", 10);
         Product bread = new Product("Bread", 4);
         Product egg = new Product("Egg", 19);
-        Product[] prods = new Product[3];
+        Product[] prods = new Product[5];
         prods[0] = milk;
         prods[1] = bread;
         prods[2] = egg;
-        for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            System.out.println(pr.getName() + " - " + pr.getCount());
-        }
-        System.out.println("Replace milk to oil.");
-        Product oil = new Product("oil", 11);
-        prods[0] = oil;
-        for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            System.out.println(pr.getName() + " - " + pr.getCount());
-        }
-        System.out.println("Shown only product.count > 10");
-        for (int index = 0; index < prods.length; index++) {
-            Product pr = prods[index];
-            if (pr.getCount() > 10) {
-                System.out.println(pr.getName() + " - " + pr.getCount());
+        for (int i = 0; i < prods.length; i++) {
+            Product product = prods[i];
+            if (product != null) {
+                System.out.println(product.getName());
             }
         }
     }
