eb729c0ed0d3dd7199932f66a84cd3c6563d3e32
JuliaSelyutina <JSSelyutina@yandex.ru>
2023-06-10 23:04:02 +0300

    0.6. Error - исключения, связанные с работой виртуальной машины [#219370 #422177].

--- src/main/java/ru/job4j/ex/FactRec.java
+++ src/main/java/ru/job4j/ex/FactRec.java
@@ -4,11 +4,8 @@ public class FactRec {
     public static int calc(int n) {
         if (n == 0 || n == 1) {
             return 1;
-        } else if (n > 1) {
-            return calc(n - 1) * n;
         }
-        System.out.println("Факториал отрицательного числа не существует!");
-        return -1;
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
