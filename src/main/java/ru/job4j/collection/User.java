931be3bffb189fee07b8f0ae709d457950544fe7
JuliaSelyutina <JSSelyutina@yandex.ru>
2024-05-10 23:52:07 +0300

    1. Организовать сортировку User [#10034].

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -31,8 +31,8 @@ public class User implements Comparable<User> {
 
     @Override
     public int compareTo(User o) {
-        if(this.name.compareTo(o.name) == 0) {
-            return Integer.compare(this.age,o.age);
+        if (this.name.compareTo(o.name) == 0) {
+            return Integer.compare(this.age, o.age);
         }
         return this.name.compareTo(o.name);
     }
